9291b5fa595db61f6dac43557b24631fc4e9865f
Manuel Castillo <mcastillo@Manuels-MacBook-Air.local>
2022-10-08 12:16:57 -0500

    Initial sketch for nav-center. Problems with the mobile view and link items when screen is resized on a desktop

---
+++ app.js
@@ -0,0 +1,8 @@
+function openmobnav() {
+    document.getElementById("navbar-link-items").style.display = "block";
+}
+
+function closemobnav() {
+    document.getElementById("navbar-link-items").style.display = "none";
+    // document.getElementById("navbar-link-items").style.display = "none";
+}
